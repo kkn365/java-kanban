98e30170a3131a5e773928ba1049fea71e03f568
Konstantin <KvitchenkoKN@yandex.ru>
2024-05-01 12:17:00 +0300

    Исправлено замечание автотеста git (Main:22).

--- src/ru/yandex/javacource/kvitchenko/schedule/Main.java
+++ src/ru/yandex/javacource/kvitchenko/schedule/Main.java
@@ -19,7 +19,7 @@ public class Main {
         final int subtask2Id = taskManager.addNewSubtask(subtask2);
 
         taskManager.getEpic(epicId);
-        for (int i = 0; i < 10; i ++) {
+        for (int i = 0; i < 10; i++) {
             taskManager.getSubtask(subtask1Id);
             taskManager.getSubtask(subtask2Id);
         }
