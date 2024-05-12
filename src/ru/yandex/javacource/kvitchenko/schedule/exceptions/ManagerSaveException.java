ea92bc8572cc24e7ade92b7b07672c0298b54563
Konstantin <KvitchenkoKN@yandex.ru>
2024-05-12 18:24:54 +0300

    Доработана функциональность по техническому заданию 7 спринта.

---
+++ src/ru/yandex/javacource/kvitchenko/schedule/exceptions/ManagerSaveException.java
@@ -0,0 +1,7 @@
+package ru.yandex.javacource.kvitchenko.schedule.exceptions;
+
+public class ManagerSaveException extends Exception {
+    public ManagerSaveException(final String message) {
+        super(message);
+    }
+}
